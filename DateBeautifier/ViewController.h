d424a251056d8dc6c71ca814562ef76cb06556fd
Thomas Edelmann <thomas@macmini.g51>
2012-08-13 11:00:37 +0200

    Initial Commit

---
+++ DateBeautifier/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DateBeautifier
+//
+//  Created by thomas on 13.08.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
